6cceb51ea74e30ef47efbb979196355e4bbb2b75
osakahinotori <malse@k-docomo.com>
2026-06-28 22:24:12 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// ===== 設定ファイル =====
+// Google Cloud で発行した OAuth クライアントID をここに貼り付けてください。
+// 取得手順は README.md を参照。
+window.APP_CONFIG = {
+  // 例: "1234567890-abcdefg.apps.googleusercontent.com"
+  GOOGLE_CLIENT_ID: "807107806694-cn7oko4irvdhj0jej04fe8d5ot081m1s.apps.googleusercontent.com",
+};
